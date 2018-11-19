0024135fafa2f8aa19e0117f235aa6ba3832757b
Christopher, Nicholas (CORP) <Nicholas.Christopher@adp.com>
2018-11-19 16:47:12 -0500

    migrating

--- build.gradle.kts
+++ build.gradle.kts
@@ -48,6 +48,37 @@ gitVersion {
     gitTagPrefix = "v"
 }
 
+val publicationName = "maven"
+publishing {
+    publications {
+        register(publicationName, MavenPublication::class) {
+            from(components["java"])
+            artifact(sourcesJar.get())
+            artifact(javadocJar.get())
+            pom.addDependencies()
+        }
+    }
+}
+
+bintray {
+    user = System.getenv("BINTRAY_USER")
+    key = System.getenv("BINTRAY_API_KEY")
+    dryRun = false
+    publish = true
+    setPublications(publicationName)
+    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
+        repo = "maven"
+        name = "$project.name"
+        desc = "Kotlin SVG generation DSL."
+        websiteUrl = "https://github.com/nwillc/ksvg"
+        issueTrackerUrl = "https://github.com/nwillc/ksvg/issues"
+        vcsUrl = "https://github.com/nwillc/ksvg.git"
+        setLicenses("ISC")
+        setLabels("kotlin","SVG","DSL")
+        publicDownloadNumbers = true
+    })
+}
+
 tasks {
     withType<KotlinCompile> {
         kotlinOptions.jvmTarget = "1.8"
@@ -55,9 +86,7 @@ tasks {
 
     withType<Test> {
         useJUnitPlatform()
-        testLogging {
-            showStandardStreams = true
-        }
+        testLogging.showStandardStreams = true
     }
 
     withType<DokkaTask> {
@@ -105,39 +134,10 @@ fun MavenPom.addDependencies() = withXml {
     }
 }
 
-val publicationName = "maven"
-publishing {
-    publications {
-        register(publicationName, MavenPublication::class) {
-            from(components["java"])
-            artifact(sourcesJar.get())
-            artifact(javadocJar.get())
-            pom.addDependencies()
-        }
-    }
-}
-
 //
 //bintrayUpload.onlyIf { !project.version.toString().contains('-') }
 //
 
-bintray {
-    user = System.getenv("BINTRAY_USER")
-    key = System.getenv("BINTRAY_API_KEY")
-    dryRun = false
-    publish = true
-    setPublications(publicationName)
-    pkg(delegateClosureOf<BintrayExtension.PackageConfig> {
-        repo = "maven"
-        name = "$project.name"
-        desc = "Kotlin SVG generation DSL."
-        websiteUrl = "https://github.com/nwillc/ksvg"
-        issueTrackerUrl = "https://github.com/nwillc/ksvg/issues"
-        vcsUrl = "https://github.com/nwillc/ksvg.git"
-        setLicenses("ISC")
-        setLabels("kotlin","SVG","DSL")
-        publicDownloadNumbers = true
-    })
-}
+
 
 
