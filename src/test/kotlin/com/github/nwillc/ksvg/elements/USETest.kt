812d580a4dc09a64bcd3494ff41c0389fd0e1dec
Nicholas Christopher <nwillc@gmail.com>
2018-11-24 17:38:13 -0500

    cleaning

--- src/test/kotlin/com/github/nwillc/ksvg/elements/USETest.kt
+++ src/test/kotlin/com/github/nwillc/ksvg/elements/USETest.kt
@@ -1,9 +1,14 @@
 /*
  * Copyright 2018 nwillc@gmail.com
  *
- * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
+ * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is
+ * hereby granted, provided that the above copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE
+ * INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE
+ * FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+ * OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING
+ * OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 package com.github.nwillc.ksvg.elements
@@ -18,12 +23,12 @@ import java.util.logging.SimpleFormatter
 import java.util.logging.StreamHandler
 
 internal class USETest : HasSvg(true) {
-    val formatter = SimpleFormatter()
-    val stream = ByteArrayOutputStream()
-    val handler = StreamHandler(stream, formatter)
+    private val formatter = SimpleFormatter()
+    private val stream = ByteArrayOutputStream()
+    private val handler = StreamHandler(stream, formatter)
 
     companion object {
-        val logger = Logger.getLogger(USE::javaClass.name)
+        val logger = Logger.getLogger(USE::javaClass.name)!!
     }
 
     @BeforeEach
