2120da5e8b47930ba520dc981967433617089f99
Nicholas Christopher <nwillc@gmail.com>
2019-04-13 22:24:32 -0400

    Dependency updates.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,12 +18,12 @@ val slf4jTestVersion: String by project
 plugins {
     jacoco
     `maven-publish`
-    kotlin("jvm") version "1.3.21"
+    kotlin("jvm") version "1.3.30"
     id("com.github.nwillc.vplugin") version "2.3.0"
     id("org.jetbrains.dokka") version "0.9.18"
     id("io.gitlab.arturbosch.detekt") version "1.0.0.RC9.2"
     id("com.jfrog.bintray") version "1.8.4"
-    id("org.jlleitschuh.gradle.ktlint") version "7.2.1"
+    id("org.jlleitschuh.gradle.ktlint") version "7.3.0"
     id("org.sonarqube") version "2.7"
 }
 
