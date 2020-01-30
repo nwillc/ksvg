72402ec00c1f4c33ec4f2d5f01335ef12000d207
Nicholas Christopher <nwillc@gmail.com>
2020-01-29 22:02:23 -0500

    Jvm tests

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,19 @@
+/*
+ * Copyright (c) 2020, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
 val coverageThreshold = 0.98
 val jvmTargetVersion = JavaVersion.VERSION_1_8.toString()
 val publicationName = "maven"
@@ -28,24 +44,32 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(kotlin("stdlib-common"))
+                listOf(
+                    kotlin("stdlib-common")
+                ).forEach { implementation(it) }
             }
         }
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+                listOf(
+                    kotlin("test-common"),
+                    kotlin("test-junit"),
+                    kotlin("test-annotations-common")
+                ).forEach { implementation(it) }
             }
         }
         val jvmMain by getting {
             dependencies {
-                implementation(kotlin("stdlib-jdk8"))
+                listOf(
+                    kotlin("stdlib-jdk8")
+                ).forEach { implementation(it) }
             }
         }
         val jvmTest by getting {
             dependencies {
-                implementation(kotlin("test"))
-                implementation(kotlin("test-junit"))
+                listOf(
+                    kotlin("test-junit")
+                ).forEach { implementation(it) }
             }
         }
     }
--- src/jvmTest/kotlin/com/github/nwillc/ksvg/FileTest.kt
+++ src/jvmTest/kotlin/com/github/nwillc/ksvg/FileTest.kt
@@ -1,19 +1,17 @@
 /*
- * Copyright 2019 nwillc@gmail.com
+ * Copyright (c) 2020, nwillc@gmail.com
  *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL
- * WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED
- * WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL
- * THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR
- * CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING
- * FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT,
- * NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION
- * WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
- *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
 package com.github.nwillc.ksvg
@@ -21,10 +19,8 @@ package com.github.nwillc.ksvg
 import com.github.nwillc.ksvg.elements.Container
 import com.github.nwillc.ksvg.elements.SVG
 import java.io.FileWriter
-import org.junit.jupiter.api.DisplayName
-import org.junit.jupiter.api.Test
+import kotlin.test.Test
 
-@DisplayName("File Output")
 class FileTest {
     @Test
     fun `create diagonal circles`() {
@@ -50,7 +46,7 @@ class FileTest {
                 }
         }
         FileWriter("/tmp/circlesDiagonal.svg").use {
-            svg.render(it, SVG.RenderMode.FILE)
+            svg.render(it, RenderMode.FILE)
         }
     }
 
@@ -109,7 +105,7 @@ class FileTest {
         }
 
         FileWriter("/tmp/codeMonkey.svg").use {
-            svg.render(it, SVG.RenderMode.FILE)
+            svg.render(it, RenderMode.FILE)
         }
     }
 
