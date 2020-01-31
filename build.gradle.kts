4aa311e50a4528285f6d30b47b0afb1c7cbcd474
NikkyAI <root@nikky.moe>
2020-01-31 18:58:06 +0100

    add publishing to mavenlocal

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@ val mockkVersion: String by project
 
 plugins {
     kotlin("multiplatform") version "1.3.61"
+    id("maven-publish")
 }
 
 group = "com.github.nwillc"
@@ -40,8 +41,20 @@ repositories {
 }
 
 kotlin {
-    jvm()
-    js()
+    jvm() {
+        mavenPublication { // Setup the publication for the target
+//            artifactId = "ksvg-jvm"
+            // Add a docs JAR artifact (it should be a custom task):
+//            artifact(javadocJar)
+        }
+    }
+    js() {
+        mavenPublication { // Setup the publication for the target
+//            artifactId = "ksvg-js"
+            // Add a docs JAR artifact (it should be a custom task):
+//            artifact(javadocJar)
+        }
+    }
     sourceSets {
         val commonMain by getting {
             dependencies {
