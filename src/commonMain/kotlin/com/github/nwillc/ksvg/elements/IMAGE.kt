ebdd0b986e8c48894c63cd15274d866860adb295
Ian Sturdy <sturdyi12@mail.wlu.edu>
2022-07-10 15:01:14 -0400

    Add image element

---
+++ src/commonMain/kotlin/com/github/nwillc/ksvg/elements/IMAGE.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2022, nwillc@gmail.com
+ *
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+
+package com.github.nwillc.ksvg.elements
+
+import com.github.nwillc.ksvg.attributes.AttributeProperty
+import com.github.nwillc.ksvg.attributes.AttributeType
+
+class IMAGE(validation: Boolean = false): Region("image", validation) {
+    var href: String? by AttributeProperty(type = AttributeType.IdName)
+    var x: String? by AttributeProperty(type = AttributeType.LengthOrPercentage)
+    var y: String? by AttributeProperty(type = AttributeType.LengthOrPercentage)
+    var height: String? by AttributeProperty(type = AttributeType.LengthOrPercentage)
+    var width: String? by AttributeProperty(type = AttributeType.LengthOrPercentage)
+}
